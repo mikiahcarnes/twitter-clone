d597e7629cade72bc595bf2a2d4a993c7267f775
mikiahcarnes <mcarnes76347@ucumberlands.edu>
2023-08-03 00:04:58 -0400

    adding layout.tsx and staticprop for slug

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,19 +1,14 @@
-import type { User } from "@clerk/nextjs/server";
-import { privateProcedure } from "~/server/api/trpc";
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import {
+  createTRPCRouter,
+  publicProcedure,
+  privateProcedure,
+} from "~/server/api/trpc";
 
-const filterUserForClient = (user: User) => {
-  return {
-    id: user.id,
-    username: user.username,
-    profileImageUrl: user.profileImageUrl,
-  };
-};
-
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
 import { Redis } from "@upstash/redis";
 
@@ -22,12 +17,6 @@ const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
   limiter: Ratelimit.slidingWindow(3, "1 m"),
   analytics: true,
-  /**
-   * Optional prefix for the keys used in redis. This is useful if you want to share a redis
-   * instance with other applications and want to avoid key collisions. The default prefix is
-   * "@upstash/ratelimit"
-   */
-  prefix: "@upstash/ratelimit",
 });
 
 export const postsRouter = createTRPCRouter({
@@ -37,14 +26,13 @@ export const postsRouter = createTRPCRouter({
       orderBy: [{ createdAt: "desc" }],
     });
 
+    const userId = posts.map((post) => post.authorId);
     const users = (
       await clerkClient.users.getUserList({
-        userId: posts.map((post) => post.authorId),
+        userId: userId,
         limit: 100,
       })
     ).map(filterUserForClient);
-
-    console.log(users);
 
     return posts.map((post) => {
       const author = users.find((user) => user.id === post.authorId);
@@ -86,6 +74,7 @@ export const postsRouter = createTRPCRouter({
           content: input.content,
         },
       });
+
       return post;
     }),
 });
