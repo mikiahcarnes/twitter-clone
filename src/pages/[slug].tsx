23441095acbc5d87e9ee807a5099621f16f33833
mikiahcarnes <mcarnes76347@ucumberlands.edu>
2023-08-04 16:27:33 -0400

    Profile page with createServerSideHelpers

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -6,6 +6,7 @@ import { prisma } from "~/server/db";
 import { appRouter } from "~/server/api/root";
 import superjson from "superjson";
 import { PageLayout } from "~/components/layout";
+import Image from "next/image";
 
 const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
   const { data } = api.profile.getUserByUsername.useQuery({
@@ -20,7 +21,19 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
         <title>{data.username}</title>
       </Head>
       <PageLayout>
-        <div>{data.username}</div>
+        <div className="relative h-36 bg-slate-600">
+          <Image
+            src={data.profileImageUrl}
+            alt={`${data.username ?? ""}'s profile picture`}
+            width={128}
+            height={128}
+            className="absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black bg-black"
+          />
+        </div>
+        <div className="h-[64px]"></div>
+        <div className="p-4 text-2xl font-bold">{`@${data.username ?? ""
+          }`}</div>
+        <div className="w-full border-b border-slate-400" />
       </PageLayout>
     </>
   );
